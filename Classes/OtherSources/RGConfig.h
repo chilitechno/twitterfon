39fa661da27ce9e26907f8394ede85b2d10a9df2
Daniel Rodríguez Troitiño <drodrigueztroitino@yahoo.es>
2009-05-06 22:13:28 +0200

    TwitterFon version 1.4.4 (rev 1726)
     LC_ALL=C svn info
    Path: .
    URL: http://naan.net/svn/trunk/TwitterFon
    Repository Root: http://naan.net/svn
    Repository UUID: 542e9493-1a22-0410-9183-c10453d2b9ee
    Revision: 1730
    Node Kind: directory
    Schedule: normal
    Last Changed Author: kaz
    Last Changed Rev: 1726
    Last Changed Date: 2009-03-14 00:53:24 +0100 (Sat, 14 Mar 2009)

---
+++ Classes/OtherSources/RGConfig.h
@@ -0,0 +1,9 @@
+#ifndef RGCONFIG
+#define RGCONFIG
+
+#define DATABASE_VERSION 20090331042358
+#define SCHEMA_VERSION 1
+#define DATABASE_LEVEL 10
+#define DATABASE_FILENAME @"geodata.sqlite"
+
+#endif
